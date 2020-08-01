8123b9646b63288ea697211eacb8773f11466022
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2020-07-31 23:35:45 -0400

    Update app.js
    Should be working again now with node.js not PHP

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Get express
 const express = require('express');
 var app = express();
 
-app.use(express.static(__dirname+'/public'));
+// Use templates
+app.use(express.static(__dirname));
 var port = process.env.PORT || 8000;
 app.listen(port);
