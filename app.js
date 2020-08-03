e0b8348d6b1e2bd1ea2a8c1eb4285c621d14dfde
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2020-08-03 10:52:00 -0400

    New scraper bundled with browserify
    script.js is the bundled script
    slight changes to other files (they actually do not matter
    package-lock.json added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 var app = express();
 
-// Use templates
+// Use templates and setup port
 app.use(express.static(__dirname));
 var port = process.env.PORT || 8000;
 app.listen(port);
