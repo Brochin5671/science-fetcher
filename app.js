06a736ed8c83543fac226c32465d32edab2708e5
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2020-08-08 18:09:34 -0400

    Added redirect to secure and redirect to home

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-// Require express
+// Require express, and setup app and port
 const express = require('express');
 var app = express();
+var port = process.env.PORT || 8000;
 
-// Serves static files without .html extension
+// Redirect to secure if request is not secure and not localhost
+if(port != 8000){
+	// Enable reverse proxy support
+	app.enable('trust proxy');
+	app.use((req,res,next) => {
+		if(req.secure) next();
+		else res.redirect('https://'+req.headers.host+req.url);
+	});
+}
+
+// Serves static files without .html extension and redirect to home if page not found
 app.use(express.static(__dirname, {
 	extensions: ['html']
-}));
+}),(req,res) => {
+	res.redirect('/');
+});
 
-// Setup port
-var port = process.env.PORT || 8000;
+// Listen to port
 app.listen(port);
