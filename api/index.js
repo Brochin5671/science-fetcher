9ae90c727ec8f3b617d4bf6b106ef0129ffdc592
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2023-08-27 22:48:19 -0400

    Ensure express api works

--- api/index.js
+++ api/index.js
@@ -16,7 +16,10 @@ app.disable('x-powered-by');
 app.listen(port, () => console.log(`Now running on ${port}`));
 
 // Serves static files without .html extension
-app.use(express.static(__dirname + '/public', { extensions: ['html'] }));
+const path = require('path');
+app.use(
+  express.static(path.resolve(__dirname, '../public'), { extensions: ['html'] })
+);
 
 // Get post request from /data, get ID, scrape information with provided ID, and return article data
 app.get('/data', (req, res) => {
@@ -156,7 +159,7 @@ function requestURL(id) {
 
 // Send 404 page if page not found, has to be last route
 app.get('*', (req, res) => {
-  res.status(404).sendFile(__dirname + '/404-page.html');
+  res.status(404).sendFile(path.resolve(__dirname, '../404-page.html'));
 });
 
 // Log server errors
